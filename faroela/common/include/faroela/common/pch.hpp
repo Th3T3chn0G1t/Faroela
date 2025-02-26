b65bd8516eab01267b632fa119750a896989623b
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2025-02-26 11:13:37 +0000

    faroela/common: add eternal to build

--- faroela/common/include/faroela/common/pch.hpp
+++ faroela/common/include/faroela/common/pch.hpp
@@ -20,7 +20,10 @@
 #include <source_location>
 #include <string_view>
 #include <system_error>
+#include <utility>
 #include <variant>
+
+#include <cstdlib>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/stopwatch.h>
@@ -29,3 +32,12 @@
 #include <spdlog/sinks/msvc_sink.h>
 
 #include <magic_enum/magic_enum.hpp>
+
+#include <mapbox/eternal.hpp>
+
+namespace faroela::common {
+	template<typename type, typename... args>
+	concept is_list_constructible = requires(args&&... v) {
+		{ type{ std::forward<args>(v)... } };
+	};
+}
