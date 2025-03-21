f4ffc0ff4f934f08a1b4ae1e089a66ec3a3021c0
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2025-03-21 22:38:37 +0000

    faroela: add common EH stub for API impl

--- faroela/src/api.cpp
+++ faroela/src/api.cpp
@@ -7,10 +7,21 @@
 
 #include <faroela/api/faroela.hpp>
 
+namespace faroela::api {
+	template<typename type>
+	static link_bool handle_base_result(faroela::context *ctx, const result <type> &result) {
+		if (!result) [[unlikely]] {
+			log_result(ctx->api_logger, result);
+			return false;
+		}
+
+		return true;
+	}
+}
+
 extern "C" {
 	namespace faroela::api {
 		// TODO: Make common root API error state/log handover.
-
 		FAROELA_COMMON_EXPORT link_bool faroela_initialize(faroela::context** ctx, [[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
 			const auto result = faroela::context::initialize();
 
@@ -28,21 +39,15 @@ extern "C" {
 		}
 
 		FAROELA_COMMON_EXPORT link_bool faroela_hid_status(faroela::context* ctx, hid::port port, link_bool connected) {
-			const auto result = ctx->submit<faroela::hid_status_event>("hid", ctx->hid.status_callback, port, !!connected);
-
-			return result.has_value();
+			return handle_base_result(ctx, ctx->submit<faroela::hid_status_event>("hid", ctx->hid.status_callback, port, !!connected));
 		}
 
 		FAROELA_COMMON_EXPORT link_bool faroela_hid_button_event(faroela::context* ctx, hid::port port, hid::button button, link_bool pressed) {
-			const auto result = ctx->submit<faroela::hid_button_event>("hid", ctx->hid.button_callback, port, button, !!pressed);
-
-			return result.has_value();
+			return handle_base_result(ctx, ctx->submit<faroela::hid_button_event>("hid", ctx->hid.button_callback, port, button, !!pressed));
 		}
 
 		FAROELA_COMMON_EXPORT link_bool faroela_hid_axis_event(faroela::context* ctx, hid::port port, hid::axis axis, float value) {
-			const auto result = ctx->submit<faroela::hid_axis_event>("hid", ctx->hid.axis_callback, port, axis, value);
-
-			return result.has_value();
+			return handle_base_result(ctx, ctx->submit<faroela::hid_axis_event>("hid", ctx->hid.axis_callback, port, axis, value));
 		}
 	}
 }
